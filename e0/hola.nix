e918e654f936259ddcf2c72114a4deebf6a70e4d
Alvar Maciel <alvarmaciel@gmail.com>
2026-09-14 17:15:13 -0300

    fix: add currentSystem to System var

--- e0/hola.nix
+++ e0/hola.nix
@@ -7,7 +7,7 @@ let
 in
 derivation {
   name = "hola";
-  system = "aarch64-darwin";
+  system = builtins.currentSystem;
   builder = "${pkgs.bash}/bin/bash";
   args = [ script ];
   coreutils = pkgs.coreutils;
