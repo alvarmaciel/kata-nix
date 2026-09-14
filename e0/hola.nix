25259caf955eb21f828e2040be29082ccadf0f92
Alvar Maciel <alvarmaciel@gmail.com>
2026-09-14 18:56:02 -0300

    add outputs

--- e0/hola.nix
+++ e0/hola.nix
@@ -1,14 +1,15 @@
 let
   pkgs = import <nixpkgs> { };
   script = builtins.toFile "echo_hola.sh" ''
-    "$coreutils/bin/ls" -la . > $out
-    echo "hola" >> $out  
+    "$coreutils/bin/ls" -la . > $doc
+    echo "hola" > $out
     '';
 in
 derivation {
   name = "hola";
   system = builtins.currentSystem;
   builder = "${pkgs.bash}/bin/bash";
+  outputs = [ "doc" "out" ];
   args = [ script ];
   coreutils = pkgs.coreutils;
 }
